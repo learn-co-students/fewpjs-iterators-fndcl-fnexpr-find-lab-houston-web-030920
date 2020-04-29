2bc2d53e8c517eaf7016310df67e7ea2230ba04d
Rooted1 <ruthobey@gmail.com>
2020-04-29 17:21:51 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+  //...
+]
+
+let superBowlWin = (resultArr) =>{
+  return resultArr.find((game)=>{
+    return game.result == "W"
+  })
+}
+// console.log(superBowlWin(record))
