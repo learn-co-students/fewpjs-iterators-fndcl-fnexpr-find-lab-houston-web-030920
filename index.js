f1ab9112b8f54e95ce42c131a126bcca859c5365
isaac-3 <isaacgc0596@gmail.com>
2020-04-29 15:00:31 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+let superbowlWin = function(arr){
+  // let result = arr.find(function(a) { return a.result === "W" })
+  // return !!result ? result.year : undefined
+  
+  let result = arr.find(function(a) {return a.result === "W" })
+  if(result){
+    return result.year
+  }else{
+    return undefined
+  }
+}
