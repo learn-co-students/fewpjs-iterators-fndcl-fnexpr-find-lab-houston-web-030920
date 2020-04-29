3ca10dfca0b33590ffe6cab66e34e07533455c42
AidanMcB <aidankmcbride@gmail.com>
2020-04-29 16:06:03 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+superbowlWin = (array) => {
+  let result = array.find( array => array.result === "W" )
+  return !!result? result.year : undefined
+
+}
